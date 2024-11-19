111f16977ff1982d85b6401fdd74cfc1d6608353
Krish-Das <krishdasatgames@gmail.com>
2024-11-19 12:54:35 +0530

    Add icons

---
+++ components/icons/material-symbols/index.ts
@@ -0,0 +1,39 @@
+import { MaterialSymbolsArrowOutward } from "./MaterialSymbolsArrowOutward"
+import { MaterialSymbolsBroadcastOnPersonalRounded } from "./MaterialSymbolsBroadcastOnPersonalRounded"
+import { MaterialSymbolsCallOutline } from "./MaterialSymbolsCallOutline"
+import { MaterialSymbolsCasesOutline } from "./MaterialSymbolsCasesOutline"
+import { MaterialSymbolsDockToLeft } from "./MaterialSymbolsDockToLeft"
+import { MaterialSymbolsDockToRight } from "./MaterialSymbolsDockToRight"
+import { MaterialSymbolsGroupOutline } from "./MaterialSymbolsGroupOutline"
+import { MaterialSymbolsInfoOutline } from "./MaterialSymbolsInfoOutline"
+import { MaterialSymbolsMagnificationLarge } from "./MaterialSymbolsMagnificationLarge"
+import { MaterialSymbolsMail } from "./MaterialSymbolsMail"
+import { MaterialSymbolsMailOutline } from "./MaterialSymbolsMailOutline"
+import { MaterialSymbolsMenu } from "./MaterialSymbolsMenu"
+import { MaterialSymbolsMenuOpen } from "./MaterialSymbolsMenuOpen"
+import { MaterialSymbolsNewspaper } from "./MaterialSymbolsNewspaper"
+import { MaterialSymbolsRectangleRounded } from "./MaterialSymbolsRectangleRounded"
+import { MaterialSymbolsWork } from "./MaterialSymbolsWork"
+import { MaterialSymbolsWorkOutline } from "./MaterialSymbolsWorkOutline"
+import { MaterialSymbolsWorkOutlineSharp } from "./MaterialSymbolsWorkOutlineSharp"
+
+export {
+  MaterialSymbolsArrowOutward,
+  MaterialSymbolsBroadcastOnPersonalRounded,
+  MaterialSymbolsCallOutline,
+  MaterialSymbolsCasesOutline,
+  MaterialSymbolsDockToLeft,
+  MaterialSymbolsDockToRight,
+  MaterialSymbolsGroupOutline,
+  MaterialSymbolsInfoOutline,
+  MaterialSymbolsMagnificationLarge,
+  MaterialSymbolsMail,
+  MaterialSymbolsMailOutline,
+  MaterialSymbolsMenu,
+  MaterialSymbolsMenuOpen,
+  MaterialSymbolsNewspaper,
+  MaterialSymbolsRectangleRounded,
+  MaterialSymbolsWork,
+  MaterialSymbolsWorkOutline,
+  MaterialSymbolsWorkOutlineSharp,
+}
---
+++ components/icons/system-ui/index.ts
@@ -0,0 +1,15 @@
+import { SystemUiconsBriefcase } from "./SystemUiconsBriefcase"
+import { SystemUiconsDisplayAlt } from "./SystemUiconsDisplayAlt"
+import { SystemUiconsEpisodes } from "./SystemUiconsEpisodes"
+import { SystemUiconsMail } from "./SystemUiconsMail"
+import { SystemUiconsNewspaper } from "./SystemUiconsNewspaper"
+import { SystemUiconsUsers } from "./SystemUiconsUsers"
+
+export {
+  SystemUiconsBriefcase,
+  SystemUiconsDisplayAlt,
+  SystemUiconsEpisodes,
+  SystemUiconsMail,
+  SystemUiconsNewspaper,
+  SystemUiconsUsers,
+}

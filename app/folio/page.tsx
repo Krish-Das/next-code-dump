463a3ab445b8a8530e8a4bcb03638dc41e87f59e
Krish-Das <krishdasatgames@gmail.com>
2024-11-19 22:50:05 +0530

    Add the scroll animation

--- app/folio/page.tsx
+++ app/folio/page.tsx
@@ -6,21 +6,42 @@ import WorkCards from "./work-cards"
 export default function Page() {
   return (
     <Main className="space-y-5 p-0">
-      <section className="flex h-dvh flex-col justify-center space-y-12">
-        <div className="space-y-3 text-center">
-          <p className="flex items-center justify-center gap-1 font-mona text-xs font-semibold uppercase text-foreground/70 font-feature-ss01 font-stretch-[115%]">
-            <span className="text-sm">
-              <MaterialSymbolsBroadcastOnPersonalRounded />
-            </span>
-            <span className="_">Knowledge and Skills</span>
-          </p>
-          <p className="font-mona text-5xl font-bold uppercase font-stretch-80 font-slant-5 font-feature-ss01">
-            Work Compilation
-          </p>
+      <section className="h-[50dvh] w-full" />
+
+      <section className="h-[200dvh]">
+        <div className="sticky top-0 mx-auto flex h-dvh flex-col items-center justify-center gap-12">
+          <div className="space-y-3">
+            <p className="flex items-center justify-center gap-1 font-mona text-xs font-semibold uppercase text-foreground/70 font-feature-ss01 font-stretch-[115%]">
+              <span className="text-sm">
+                <MaterialSymbolsBroadcastOnPersonalRounded />
+              </span>
+              <span className="_">Knowledge and Skills</span>
+            </p>
+            <p className="font-mona text-5xl font-bold uppercase font-stretch-80 font-slant-5 font-feature-ss01">
+              Work Compilation
+            </p>
+          </div>
 
           <WorkCards />
         </div>
       </section>
+
+      <section className="h-[50dvh] w-full" />
+
+      {/* <section className="flex h-dvh flex-col justify-center space-y-12"> */}
+      {/*   <div className="space-y-3 text-center"> */}
+      {/*     <p className="flex items-center justify-center gap-1 font-mona text-xs font-semibold uppercase text-foreground/70 font-feature-ss01 font-stretch-[115%]"> */}
+      {/*       <span className="text-sm"> */}
+      {/*         <MaterialSymbolsBroadcastOnPersonalRounded /> */}
+      {/*       </span> */}
+      {/*       <span className="_">Knowledge and Skills</span> */}
+      {/*     </p> */}
+      {/*     <p className="font-mona text-5xl font-bold uppercase font-stretch-80 font-slant-5 font-feature-ss01"> */}
+      {/*       Work Compilation */}
+      {/*     </p> */}
+      {/*     <WorkCards /> */}
+      {/*   </div> */}
+      {/* </section> */}
     </Main>
   )
 }
--- app/folio/work-cards.tsx
+++ app/folio/work-cards.tsx
@@ -1,47 +1,79 @@
 "use client"
 
-import { motion } from "motion/react"
+import { motion, useScroll, useTransform } from "motion/react"
+
+const nameVariant = {
+  fromLeft: {
+    top: 0,
+    left: "calc((100% + 1.25rem) * -0)",
+    rotate: -5,
+  },
+  toLeft: {
+    top: 0,
+    left: "calc((100% + 1.25rem) * -1)",
+    rotate: 0,
+  },
+  fromRight: {
+    top: 0,
+    left: "calc((100% + 1.25rem) * -0)",
+    rotate: 5,
+  },
+  toRight: {
+    top: 0,
+    left: "calc((100% + 1.25rem) * 1)",
+    rotate: 0,
+  },
+}
 
 export default function WorkCards() {
-  const nameVariant = {
-    fromLeft: {
-      top: 0,
-      left: "calc((100% + 1.25rem) * -0)",
-      rotate: -5,
-    },
-    toLeft: {
-      top: 0,
-      left: "calc((100% + 1.25rem) * -1)",
-      rotate: 0,
-    },
-    fromRight: {
-      top: 0,
-      left: "calc((100% + 1.25rem) * -0)",
-      rotate: 5,
-    },
-    toRight: {
-      top: 0,
-      left: "calc((100% + 1.25rem) * 1)",
-      rotate: 0,
-    },
-  }
+  const { scrollYProgress } = useScroll()
+  const translateLeft = useTransform(
+    scrollYProgress,
+    [0, 1],
+    ["calc((100% + 1.25rem) * 0)", "calc((100% + 1.25rem) * -1)"]
+  )
+  const translateRight = useTransform(
+    scrollYProgress,
+    [0, 1],
+    ["calc((100% + 1.25rem) * 0)", "calc((100% + 1.25rem) * 1)"]
+  )
+  const rotateLeft = useTransform(scrollYProgress, [0, 1], [-5, 0])
+  const rotateRight = useTransform(scrollYProgress, [0, 1], [5, 0])
 
   return (
     <div className="relative mx-auto flex w-fit items-center justify-center gap-5">
       <motion.div
         className="left-card absolute h-72 w-52 origin-bottom rounded-md border border-[#242422] bg-[#131312]"
-        style={nameVariant.fromLeft}
-        animate={nameVariant.toLeft}
-        initial={nameVariant.fromLeft}
+        style={{
+          top: 0,
+          x: translateRight,
+          rotate: rotateRight,
+        }}
       />
+
       <motion.div
         className="left-card absolute h-72 w-52 origin-bottom rounded-md border border-[#242422] bg-[#131312]"
-        style={nameVariant.fromRight}
-        animate={nameVariant.toRight}
-        initial={nameVariant.fromRight}
+        style={{
+          top: 0,
+          x: translateLeft,
+          rotate: rotateLeft,
+        }}
       />
 
       <div className="relative h-72 w-52 rounded-md border border-[#242422] bg-[#131312]" />
     </div>
   )
 }
+
+// <motion.div
+//   className="left-card absolute hidden h-72 w-52 origin-bottom rounded-md border border-[#242422] bg-[#131312]"
+//   style={nameVariant.fromLeft}
+//   animate={nameVariant.toLeft}
+//   initial={nameVariant.fromLeft}
+// />
+// <motion.div
+//   className="left-card absolute hidden h-72 w-52 origin-bottom rounded-md border border-[#242422] bg-[#131312]"
+//   style={nameVariant.fromRight}
+//   animate={nameVariant.toRight}
+//   initial={nameVariant.fromRight}
+// />

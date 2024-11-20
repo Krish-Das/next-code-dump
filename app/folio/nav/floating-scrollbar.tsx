c04d2cd48f583c9fefd909193c3b5f2560081da3
Krish-Das <krishdasatgames@gmail.com>
2024-11-20 22:43:34 +0530

    add hover animation to the floating scrollbar

--- app/folio/nav/floating-scrollbar.tsx
+++ app/folio/nav/floating-scrollbar.tsx
@@ -8,17 +8,23 @@ export default function FloatingScrollbar() {
   const height = useTransform(scrollYProgress, [0, 1], ["0%", "100%"])
 
   return (
-    <div className="overflow-y-hiddens fixed right-8 top-1/2 h-32 -translate-y-1/2 rounded-full bg-[#21211f]">
+    <motion.div
+      className="fixed right-8 top-1/2 z-20 h-32 w-0.5 -translate-y-1/2 overflow-y-hidden rounded-none bg-[#21211f]"
+      whileHover={{
+        borderRadius: 5,
+        width: 30,
+      }}
+    >
       <motion.div
-        className="h-[25%]s relative w-0.5 origin-top rounded-full bg-white/80"
+        className="relative origin-top bg-white/80"
         style={{
           height,
           background:
             "linear-gradient(to bottom, rgba(255, 255, 255, 0.7), white)",
         }}
       >
-        <span className="absolute bottom-0 left-1/2 size-1 -translate-x-1/2 rounded-full bg-white/90 blur-sm" />
+        {/* <span className="absolute bottom-0 left-1/2 size-1 -translate-x-1/2 rounded-full bg-white/90 blur-sm" /> */}
       </motion.div>
-    </div>
+    </motion.div>
   )
 }

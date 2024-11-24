4a4dfd83c8de14798f337516f544a59fee5701d4
Krish-Das <krishdasatgames@gmail.com>
2024-11-24 16:27:47 +0530

    add cards

--- app/test/skills-scroll/horizontal-test-2.tsx
+++ app/test/skills-scroll/horizontal-test-2.tsx
@@ -1,5 +1,7 @@
 "use client"
 
+import { motion } from "motion/react"
+
 import { cn } from "@/lib/utils"
 
 export default function Test2() {
@@ -15,9 +17,26 @@ export default function Test2() {
 
 function Sticky() {
   return (
-    <div className="container grid h-[90vh] w-full place-items-center bg-grey-2 p-0">
-      thing
+    <div className="overflow-x-hiddens container sticky top-0 flex h-[100vh] w-full items-center bg-grey-2 p-0">
+      <motion.div className="flex h-48 w-full bg-black" style={{ x: 0 }}>
+        {Array.from({ length: 3 }, (_, idx) => (
+          <SkillCard key={idx} id={idx} />
+        ))}
+      </motion.div>
     </div>
+  )
+}
+
+function SkillCard({ id }: { id: number }) {
+  const colors = ["bg-blue-300", "bg-amber-300", "bg-green-300"]
+
+  return (
+    <div
+      className={cn(
+        "h-48 w-[42.5rem] flex-shrink-0 rounded-xl bg-grey-2",
+        colors[id]
+      )}
+    />
   )
 }
 
@@ -31,7 +50,7 @@ function Ghost({
   return (
     // NOTE: ghost won't have any child and only will have height
     <div
-      className={cn(showGhost && "flex justify-center")}
+      className={cn("ghost-scroller", showGhost && "flex justify-center")}
       style={{
         height: height || "100vh",
       }}

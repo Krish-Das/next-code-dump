2e72bb18a12a2ca4325d9c1aae36d48137e279c3
Krish-Das <krishdasatgames@gmail.com>
2024-11-24 23:35:38 +0530

    add child of cards

--- app/test/skills-cards/Cards.tsx
+++ app/test/skills-cards/Cards.tsx
@@ -1,4 +1,5 @@
 import { cn } from "@/lib/utils"
+import { MaterialSymbolsRectangleRounded } from "@/components/icons/material-symbols"
 
 export default function Cards() {
   return (
@@ -9,20 +10,42 @@ export default function Cards() {
 
       <Grid className="container">
         <SkillCards className="col-start-2" />
-        <SkillCards />
+        <SkillCards isHovering />
         <SkillCards />
       </Grid>
     </section>
   )
 }
-function SkillCards({ className }: { className?: string }) {
+function SkillCards({
+  className,
+  isHovering,
+}: {
+  className?: string
+  isHovering?: boolean
+}) {
   return (
     <div
       className={cn(
-        "col-span-2 h-[30rem] w-full rounded-md border border-grey-1 bg-grey-1/20",
+        "col-span-2 flex h-[30rem] w-full flex-col gap-1.5 rounded-md border border-grey-1 bg-grey-1/20 p-1.5",
         className
       )}
-    />
+    >
+      <div className="grid flex-1 place-items-center rounded text-foreground/80">
+        <MaterialSymbolsRectangleRounded />
+      </div>
+      {isHovering && (
+        <div
+          className="grid h-16 grid-cols-2 gap-3 rounded-md bg-grey-1
+        font-mona text-[0.6rem] font-semibold uppercase tracking-wider text-foreground/80 font-feature-ss01 font-stretch-[115%]
+        "
+        >
+          <p className="self-center text-center">2020</p>
+          <p className="self-center">
+            This is some
+          </p>
+        </div>
+      )}
+    </div>
   )
 }
 

4b45cc409f132f4068588adbe912687bf3325efd
Krish-Das <krishdasatgames@gmail.com>
2024-11-24 17:53:30 +0530

    add debounce function

--- lib/utils.ts
+++ lib/utils.ts
@@ -1,12 +1,37 @@
-import { clsx, type ClassValue } from "clsx";
-import { twMerge } from "tailwind-merge";
+import { clsx, type ClassValue } from "clsx"
+import { twMerge } from "tailwind-merge"
 
 export function cn(...inputs: ClassValue[]) {
-  return twMerge(clsx(inputs));
+  return twMerge(clsx(inputs))
 }
 
 export function wait(milliseconds: number) {
   return new Promise((resolve) => {
-    setTimeout(resolve, milliseconds);
-  });
+    setTimeout(resolve, milliseconds)
+  })
+}
+
+// eslint-disable-next-line no-unused-vars
+export function debounce<T extends (...args: any[]) => any>(
+  func: T,
+  wait: number
+): T & { cancel: () => void } {
+  let timeout: ReturnType<typeof setTimeout> | null = null
+
+  const debounced = (...args: Parameters<T>) => {
+    if (timeout) {
+      clearTimeout(timeout)
+    }
+    timeout = setTimeout(() => {
+      func(...args)
+    }, wait)
+  }
+
+  debounced.cancel = () => {
+    if (timeout) {
+      clearTimeout(timeout)
+    }
+  }
+
+  return debounced as T & { cancel: () => void }
 }

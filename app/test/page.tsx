bcbca1200827f976687f1aa12bae7fd9a0bb9e00
Krish-Das <krishdasatgames@gmail.com>
2024-11-24 15:52:19 +0530

    export the placeholder

--- app/test/page.tsx
+++ app/test/page.tsx
@@ -18,7 +18,7 @@ export default function Page() {
   )
 }
 
-function PlaceHolder({ children }: { children?: React.ReactNode }) {
+export function PlaceHolder({ children }: { children?: React.ReactNode }) {
   return (
     <section className="h-[50dvh]s grid h-screen place-items-center">
       {children}

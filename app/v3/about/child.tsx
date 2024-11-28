f986f30d1d2934466c1eacd607c19cd2e1c37e12
Krish-Das <krishdasatgames@gmail.com>
2024-11-28 21:41:22 +0530

    add about section

--- app/v3/about/child.tsx
+++ app/v3/about/child.tsx
@@ -1,30 +1,58 @@
+import { cn } from "@/lib/utils"
 import { Grid } from "@/app/test/skills-cards/Cards"
 
 export default function Child() {
   return (
     <section className="container flex h-screen flex-col items-center justify-center p-0">
       <Grid className="">
-        <div className="col-span-full col-start-4 flex w-full max-w-[41ch] flex-col gap-16 text-sm">
+        <div className="col-span-full col-start-4 flex w-full max-w-[41ch] flex-col gap-16 text-sm text-foreground/90">
           <p>
-            Over the course of <em>my career</em>,<br />
-            I&apos;ve strived to develop a <em>diverse</em> set of core skills.
+            Over the course of <GambarinoEm>my career</GambarinoEm>,<br />
+            I&apos;ve strived to develop a <GambarinoEm>
+              diverse
+            </GambarinoEm>{" "}
+            set of core skills.
             <br />
-            Ever since I started working with <em>WebGL and Shaders</em>.
+            Ever since I started working with{" "}
+            <GambarinoEm>WebGL and Shaders</GambarinoEm>.
           </p>
 
           <p className="">
             Hi! I&apos;m Maxime, a frontend engineer based in New York. Welcome
             to my corner of the Internet, where I showcase my work, craft,
-            unfinished or imperfect projects, and the many other things I&apos;m
-            exploring.
+            <GambarinoEm> unfinished or imperfect projects</GambarinoEm>, and
+            the many other things I&apos;m exploring.
           </p>
 
           <p>
-            Ever since I started working with <em>WebGL and Shaders</em>.
+            Ever since I started working with{" "}
+            <GambarinoEm>WebGL and Shaders</GambarinoEm>.
           </p>
         </div>
       </Grid>
     </section>
+  )
+}
+
+export function GambarinoEm({
+  children,
+  className,
+}: {
+  children: React.ReactNode
+  className?: string
+}) {
+  return (
+    <em
+      className={cn(
+        "text-base font-normal not-italic text-foreground",
+        className
+      )}
+      style={{
+        fontFamily: "var(--font-gambarino), serif",
+      }}
+    >
+      {children}
+    </em>
   )
 }
 

2b92f79a79dfe058ae2d57833f4e8974fe672412
Krish-Das <krishdasatgames@gmail.com>
2024-11-29 14:57:46 +0530

    add basic layout

--- app/v3/combined/page.tsx
+++ app/v3/combined/page.tsx
@@ -1,13 +1,12 @@
 import { MaterialSymbolsCircle } from "@/components/icons/material-symbols"
-import Nav from "@/app/folio/nav"
 import { BgLines } from "@/app/test/int/Component"
 
-import { Heading, Para, Subheading } from "./components"
+import { Grid, Heading, Para, Subheading } from "./components"
 
 export default function Page() {
   return (
     <>
-      <Nav />
+      {/* <Nav /> */}
 
       <Child />
 
@@ -19,20 +18,41 @@ export default function Page() {
 function Child() {
   return (
     <>
-      <section className="space-y-8 bg-grn-2/40">
-        <Subheading>
+      <Grid className="gap-y-8 bg-grn-2/60">
+        <Subheading className="col-span-2 col-start-4">
           <MaterialSymbolsCircle />
           Shaders Light effects
         </Subheading>
-        <Para>
+
+        <Para className="col-span-3 col-start-4">
           Over the course of my career, <br />
           Ive strived to develop a diverse set of core skills.
           <br />
           Ever since I started working with <em>WebGL and Shaders</em>.
         </Para>
-        <Heading>Known Technologies</Heading>
-        <div className="h-32 w-full rounded-md bg-grey-1" />
-      </section>
+
+        <Heading className="col-span-3 col-start-4">Education</Heading>
+
+        <div className="col-start-2 -col-end-2 h-32 w-full rounded-md bg-grey-1" />
+      </Grid>
+
+      <Grid className="gap-y-8 bg-grn-2/40">
+        <Subheading className="col-span-2 col-start-4">
+          <MaterialSymbolsCircle />
+          Space and Rocket
+        </Subheading>
+
+        <Para className="col-span-3 col-start-4">
+          Over the course of my career, <br />
+          Ive strived to develop a diverse set of core skills.
+          <br />
+          Ever since I started working with <em>WebGL and Shaders</em>.
+        </Para>
+
+        <Heading className="col-span-3 col-start-3">Known Technologies</Heading>
+
+        <div className="col-start-2 -col-end-2 h-32 w-full rounded-md bg-grey-1" />
+      </Grid>
     </>
   )
 }

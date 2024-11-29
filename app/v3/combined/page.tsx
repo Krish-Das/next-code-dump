4652fd8d7ae9772b9976df945b054fd743b12a32
Krish-Das <krishdasatgames@gmail.com>
2024-11-29 15:12:49 +0530

    add link component in skills

--- app/v3/combined/page.tsx
+++ app/v3/combined/page.tsx
@@ -1,4 +1,9 @@
-import { MaterialSymbolsCircle } from "@/components/icons/material-symbols"
+import Link from "next/link"
+
+import {
+  MaterialSymbolsArrowForward,
+  MaterialSymbolsCircle,
+} from "@/components/icons/material-symbols"
 import { BgLines } from "@/app/test/int/Component"
 
 import { Grid, Heading, Para, Subheading } from "./components"
@@ -33,10 +38,10 @@ function Child() {
 
         <Heading className="col-span-3 col-start-4">Education</Heading>
 
-        <div className="col-start-2 -col-end-2 h-32 w-full rounded-md bg-grey-1" />
+        <div className="col-start-2 -col-end-2 h-32 rounded-md bg-grey-1" />
       </Grid>
 
-      <Grid className="gap-y-8 bg-grn-2/40">
+      <Grid className="gap-y-8 bg-grn-2/20">
         <Subheading className="col-span-2 col-start-4">
           <MaterialSymbolsCircle />
           Space and Rocket
@@ -51,7 +56,17 @@ function Child() {
 
         <Heading className="col-span-3 col-start-3">Known Technologies</Heading>
 
-        <div className="col-start-2 -col-end-2 h-32 w-full rounded-md bg-grey-1" />
+        <div className="inline-flex items-center justify-end">
+          <Link
+            href="#"
+            className="inline-flex h-fit items-center gap-2 rounded-md bg-grn-1 px-2.5 py-1 pr-2 font-gambarino text-sm text-[#E2FCDB] [&>svg]:text-base [&>svg]:text-brand-green"
+          >
+            Do something
+            <MaterialSymbolsArrowForward />
+          </Link>
+        </div>
+
+        <div className="col-start-2 -col-end-2 h-32 rounded-md bg-grey-1" />
       </Grid>
     </>
   )

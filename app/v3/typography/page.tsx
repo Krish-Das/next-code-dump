6389cf14075175ac0b57c9e9838659273d6e59a8
Krish-Das <krishdasatgames@gmail.com>
2024-12-04 14:36:19 +0530

    add nav and interview container

--- app/v3/typography/page.tsx
+++ app/v3/typography/page.tsx
@@ -1,4 +1,9 @@
-import { MaterialSymbolsBook4Spark } from "@/components/icons/material-symbols"
+import {
+  MaterialSymbolsArrowForward,
+  MaterialSymbolsBook4Spark,
+  MaterialSymbolsUpcomingOutline,
+} from "@/components/icons/material-symbols"
+import { Grid } from "@/app/test/skills-cards/Cards"
 
 import {
   DualFontHeading,
@@ -6,99 +11,128 @@ import {
   Subheading,
   TertiaryHeading,
 } from "./components/heading"
+import Navbar from "./components/navbar"
 import { Spacing } from "./components/spacing"
 import { Paragraph } from "./components/text"
 
 export default function Page() {
   return (
-    <div className="bg-black">
-      {/* <div className="h-24 w-96 rounded-md bg-grey-2" /> */}
+    <>
+      <Navbar />
 
-      <Spacing size="section" />
+      <div className="bg-black">
+        {/* <div className="h-24 w-96 rounded-md bg-grey-2" /> */}
 
-      <section className="flex flex-col items-center">
-        <TertiaryHeading className="text-[#CDFCBC]" textSmall>
-          Moebius-Style post-processing
-        </TertiaryHeading>
+        <Spacing size="section" />
 
-        <Spacing size="sm" />
+        <section className="flex flex-col items-center">
+          <TertiaryHeading className="text-[#CDFCBC]" textSmall>
+            Moebius-Style post-processing
+          </TertiaryHeading>
 
-        <Paragraph center>
-          Over the course of my career, I&apos;ve
-          <br /> strived to develop a diverse set of core skills.
-          <br /> Ever since I started working with WebGL and Shaders.
-        </Paragraph>
-      </section>
+          <Spacing size="sm" />
 
-      <Spacing size="lg" />
+          <Paragraph center>
+            Over the course of my career, I&apos;ve
+            <br /> strived to develop a diverse set of core skills.
+            <br /> Ever since I started working with WebGL and Shaders.
+          </Paragraph>
+        </section>
 
-      <section className="flex flex-col items-center">
-        <Subheading>
-          <MaterialSymbolsBook4Spark />
-          Skills at disposal
-        </Subheading>
+        <Spacing size="lg" />
 
-        <Spacing />
+        <section className="flex flex-col items-center">
+          <Subheading>
+            <MaterialSymbolsBook4Spark />
+            Skills at disposal
+          </Subheading>
 
-        <PrimaryHeading>
-          What we&apos;re
-          <br />
-          <span className="ml-[3ch]">Good at</span>
-        </PrimaryHeading>
+          <Spacing />
 
-        <Spacing size="md" />
+          <PrimaryHeading>
+            What we&apos;re
+            <br />
+            <span className="ml-[3ch]">Good at</span>
+          </PrimaryHeading>
 
-        <TertiaryHeading className="text-[#CDFCBC]" textSmall>
-          Moebius-Style post-processing
-        </TertiaryHeading>
+          <Spacing size="md" />
 
-        <Spacing />
+          <TertiaryHeading className="text-[#CDFCBC]" textSmall>
+            Moebius-Style post-processing
+          </TertiaryHeading>
 
-        <div className="flex items-center justify-center gap-1">
-          <div className="h-80 w-72 rounded-md bg-grey-1" />
-          <div className="h-80 w-72 rounded-md bg-grey-1" />
-        </div>
-      </section>
+          <Spacing />
 
-      <Spacing size="section" />
+          <div className="flex items-center justify-center gap-1">
+            <div className="h-80 w-72 rounded-md bg-grey-1" />
+            <div className="h-80 w-72 rounded-md bg-grey-1" />
+          </div>
+        </section>
 
-      <section className="flex flex-col items-center">
-        <Subheading className="text-[#CDFCBC]">
-          <MaterialSymbolsBook4Spark />
-          Skills at disposal
-        </Subheading>
+        <Spacing size="section" />
 
-        <DualFontHeading>
-          Known <em>technologies.</em>
-        </DualFontHeading>
+        <section className="flex flex-col items-center">
+          <Subheading className="text-[#CDFCBC]">
+            <MaterialSymbolsBook4Spark />
+            Skills at disposal
+          </Subheading>
 
-        <Spacing />
+          <DualFontHeading>
+            Known <em>technologies.</em>
+          </DualFontHeading>
 
-        <div className="mb-1 flex h-48 w-[50vw] items-center justify-center gap-1">
-          <div className="h-full w-1/2 rounded-md border-2 border-grey-3 bg-grey-2" />
-          <div className="h-full w-1/2 rounded-md border-2 border-grey-2 bg-grey-1" />
-        </div>
-        <div className="h-56 w-[50vw] rounded-md border-2 border-grey-2 bg-grey-1" />
-      </section>
+          <Spacing />
 
-      <Spacing size="section" />
+          <div className="mb-1 flex h-48 w-[50vw] items-center justify-center gap-1">
+            <div className="h-full w-1/2 rounded-md border-2 border-grey-3 bg-grey-2" />
+            <div className="h-full w-1/2 rounded-md border-2 border-grey-2 bg-grey-1" />
+          </div>
+          <div className="h-56 w-[50vw] rounded-md border-2 border-grey-2 bg-grey-1" />
+        </section>
 
-      <section className="flex flex-col items-center">
-        <Subheading className="text-[#CDFCBC]">
-          <MaterialSymbolsBook4Spark />
-          Skills at disposal
-        </Subheading>
+        <Spacing size="section" />
 
-        <DualFontHeading>
-          Upcoming <em>interviews.</em>
-        </DualFontHeading>
+        <section className="flex flex-col items-center">
+          <Subheading className="text-[#CDFCBC]">
+            <MaterialSymbolsBook4Spark />
+            Skills at disposal
+          </Subheading>
 
-        <Spacing />
+          <DualFontHeading>
+            Upcoming <em>interviews.</em>
+          </DualFontHeading>
 
-        <div className="h-80 w-[50vw] rounded-md bg-grey-2" />
-      </section>
+          <Spacing />
 
-      <Spacing size="lg" />
-    </div>
+          {/* <div className="h-80 w-[50vw] rounded-md bg-grey-2" /> */}
+          <Grid className="container w-full p-0">
+            <div className="col-span-4 col-start-3 space-y-3 rounded-md border-2 border-grey-2 bg-grey-1 p-2">
+              <div
+                className="
+              pointer-events-none flex h-40 touch-none select-none items-center justify-center gap-2
+              font-mona text-xs font-semibold uppercase leading-none text-[#666661] font-feature-ss01 font-stretch-[115%]
+              [&>svg]:mb-[0.3ch] [&>svg]:text-lg
+              "
+              >
+                <MaterialSymbolsUpcomingOutline />
+                <p>No interview yet</p>
+              </div>
+
+              <button
+                className="
+                inline-flex h-12 w-full items-center justify-center gap-1.5 rounded-md
+                bg-gradient-to-t from-[#B5B5B5] to-[#CDCDCD] font-medium text-[#0A0A0A] [&>svg]:text-lg
+                "
+              >
+                <MaterialSymbolsArrowForward />
+                Scedule an Interview
+              </button>
+            </div>
+          </Grid>
+        </section>
+
+        <Spacing size="lg" />
+      </div>
+    </>
   )
 }

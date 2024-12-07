b354765580536ffd4c0c89aeb53fc033c2780ec6
Krish-Das <krishdasatgames@gmail.com>
2024-12-07 19:54:24 +0530

    Add interview section
    MAIN CHANGES:
    
    1. add Interview section
    2. add custom selection background-color

--- app/v3/bench/page.tsx
+++ app/v3/bench/page.tsx
@@ -1,4 +1,9 @@
-import { MaterialSymbolsGroupOutline } from "@/components/icons/material-symbols"
+import { SprinkleSvg } from "@/components/icons/custom"
+import {
+  MaterialSymbolsArrowForward,
+  MaterialSymbolsGroupOutline,
+  MaterialSymbolsUpcomingOutline,
+} from "@/components/icons/material-symbols"
 import { Grid } from "@/app/test/skills-cards/Cards"
 
 import { Subheading, TertiaryHeading } from "../typography/components/heading"
@@ -20,6 +25,9 @@ export default function Page() {
 
         <Spacing size="lg" />
         <OtherProps />
+
+        <Spacing size="lg" />
+        <Interviews />
 
         <Spacing size="nav" />
       </div>
@@ -186,5 +194,73 @@ function OtherProps() {
         ))}
       </div>
     </Grid>
+  )
+}
+
+function Interviews() {
+  return (
+    <>
+      <Grid className="grid-cols-12 gap-0 px-8">
+        {/* FIRST ROW */}
+        <div className="col-span-full col-start-4 mb-28">
+          <h2 className="relative w-fit font-mona text-6xl font-bold leading-none tracking-tight text-[#e1e3fc]/50">
+            <span className="font-red font-normal">Upcoming</span> Inte
+            <span className="tracking-wide">r</span>views
+            <span className="absolute right-[0%] top-[20%] size-12 origin-bottom-left -translate-y-1/2 translate-x-2/3 text-brand-green">
+              <SprinkleSvg />
+            </span>
+          </h2>
+        </div>
+
+        {/* SECOND ROW */}
+        <div className="col-span-3 col-start-4 flex flex-col gap-3">
+          <Subheading className="font-semibold text-[#e1e3fc]/50">
+            The Colophone
+          </Subheading>
+
+          <p className="max-w-[29ch] font-mona text-xs text-[#e1e3fc] font-feature-ss01 font-stretch-[105%]">
+            The random, the beautiful, the weird, the temporary, the evergreen.
+          </p>
+        </div>
+
+        <div className="col-span-3 flex flex-col gap-3 font-mona text-xs text-[#e1e3fc] font-feature-ss01 font-stretch-[105%]">
+          <Spacing className="h-4" />
+
+          <p className="max-w-[29ch]">
+            Ever since I started working with WebGL and Shaders.
+          </p>
+
+          <p className="max-w-[44ch]">
+            Over the course of my career, I&apos;ve strived to develop a diverse
+            set of core skills. Ever since I started working with WebGL and
+            Shaders.
+          </p>
+        </div>
+
+        {/* THIRD ROW */}
+        <div className="col-start-4 -col-end-4 mt-24 gap-1 space-y-1 rounded-md border border-white/5 bg-grey-1 p-1">
+          <div
+            className="
+          pointer-events-none flex h-[15.5rem] touch-none select-none items-center justify-center gap-2
+          font-mona text-xs font-semibold uppercase leading-none text-[#666661] font-feature-ss01 font-stretch-[115%]
+          [&>svg]:mb-[0.3ch] [&>svg]:text-lg
+          "
+          >
+            <MaterialSymbolsUpcomingOutline />
+            <p>No interview yet</p>
+          </div>
+
+          <button
+            className="inline-flex h-10 w-full items-center justify-center gap-1 rounded bg-grey-2/70 font-mona text-sm
+          font-medium text-foreground/80 font-feature-ss01 font-stretch-[105%]
+          [&>svg]:text-base [&>svg]:text-foreground/60
+          "
+          >
+            <MaterialSymbolsArrowForward />
+            Scedule an Interview
+          </button>
+        </div>
+      </Grid>
+    </>
   )
 }

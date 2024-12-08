386f348c361f7470b61e44d0d34e89f46c41ae0c
Krish-Das <krishdasatgames@gmail.com>
2024-12-08 13:30:54 +0530

    add reveal animation to the dot

--- app/v3/bench/developement-indicator.tsx
+++ app/v3/bench/developement-indicator.tsx
@@ -1,3 +1,8 @@
+"use client"
+
+import { useState } from "react"
+import { motion } from "motion/react"
+
 import { MaterialSymbolsConstructionRounded } from "@/components/icons/material-symbols"
 
 export default function DevelopementIndicator() {
@@ -10,19 +15,42 @@ export default function DevelopementIndicator() {
 
 // TODO: Rename later
 function Dot2() {
+  const [isHovering, setHovering] = useState(false)
+
   return (
     <div
       className="
-      gap-1.5s inline-flex h-8 w-fit min-w-8 select-none items-center justify-center rounded-full bg-yellow-700/30
-      px-2 text-[0.65rem] text-xs leading-none text-yellow-200 backdrop-blur-sm [&>svg]:text-sm
+      inline-flex h-8 w-fit min-w-8 select-none items-center rounded-full bg-yellow-700/30
+      px-2 text-[0.65rem] text-yellow-200 backdrop-blur-sm [&>svg]:text-sm
         "
+      onMouseEnter={() => setHovering(true)}
+      onMouseLeave={() => setHovering(false)}
     >
       <MaterialSymbolsConstructionRounded />
-      <div className="w-0s flex w-auto flex-nowrap overflow-hidden">
-        <p className="text-nowrap pl-1.5 font-mona font-medium tracking-wide font-stretch-110 font-feature-ss01">
+      <motion.div
+        className="flex w-auto flex-nowrap overflow-hidden"
+        variants={{
+          hidden: { width: 0 },
+          visible: { width: "auto" },
+        }}
+        initial="hidden"
+        animate={isHovering ? "visible" : "hidden"}
+        // animate="visible"
+        // transition={{ type: "spring", stiffness: 100, damping: 10, mass: 1 }}
+      >
+        <motion.p
+          className="text-nowrap pl-1.5 font-mona font-medium tracking-wide font-stretch-110 font-feature-ss01"
+          variants={{
+            hidden: { filter: "blur(2px)", opacity: 0, y: "100%" },
+            visible: { filter: "blur(0px)", opacity: 1, y: "0%" },
+          }}
+          initial="hidden"
+          animate={isHovering ? "visible" : "hidden"}
+          transition={{ duration: 0.1, bounce: 0 }}
+        >
           Development preview
-        </p>
-      </div>
+        </motion.p>
+      </motion.div>
     </div>
   )
 }

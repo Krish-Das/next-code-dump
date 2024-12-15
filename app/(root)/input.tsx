ca9c1cefe8e26bb906e771939b9f0245be0dc94a
Krish-Das <krishdasatgames@gmail.com>
2024-12-15 20:20:44 +0530

    add country-displa-component

--- app/(root)/input.tsx
+++ app/(root)/input.tsx
@@ -5,7 +5,7 @@ import { TextField, Label, Input as RacInput } from "react-aria-components"
 
 export default function Input() {
   return (
-    <div className="">
+    <div className="space-y-1">
       <TextField className="flex h-11 w-[35ch] items-center rounded-lg bg-[#767680]/25 outline-none">
         <Label className="inline-flex h-full items-center pl-3 pr-1.5">
           <span className="sr-only">Search country</span>
@@ -21,6 +21,32 @@ export default function Input() {
           style={{ caretColor: "#0a84ff" }}
         />
       </TextField>
+
+      <CountryDisplay />
+    </div>
+  )
+}
+
+function CountryDisplay() {
+  const countries: string[] = ["india", "us", "something else"]
+  const time = 200
+
+  return (
+    <div className="">
+      <ul className="max-h-64 min-h-0 w-full overflow-y-scroll rounded-lg bg-[#1c1c1e] font-medium capitalize">
+        {countries.map((country, idx) => (
+          <li
+            key={idx}
+            className="flex h-11 w-full items-center border-b-[#545458]/60 px-4 [&:not(:last-child)]:border-b"
+          >
+            {country}
+          </li>
+        ))}
+      </ul>
+
+      <div className="flex h-9 items-center justify-end pr-1 text-right text-xs font-medium text-foreground/40">
+        <p>Results in {time}ms</p>
+      </div>
     </div>
   )
 }

7e2411c475076bc8ef4dd2f757fa95240498fa9e
Krish-Das <krishdasatgames@gmail.com>
2024-12-16 12:38:58 +0530

    add `wait` and `debounce` util functions

--- lib/utils.ts
+++ lib/utils.ts
@@ -4,3 +4,33 @@ import { twMerge } from "tailwind-merge"
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs))
 }
+
+export function wait(milliseconds: number) {
+  return new Promise(resolve => {
+    setTimeout(resolve, milliseconds)
+  })
+}
+
+export function debounce<T extends (...args: unknown[]) => unknown>(
+  func: T,
+  wait: number
+): T & { cancel: () => void } {
+  let timeout: ReturnType<typeof setTimeout> | null = null
+
+  const debounced = (...args: Parameters<T>) => {
+    if (timeout) {
+      clearTimeout(timeout)
+    }
+    timeout = setTimeout(() => {
+      func(...args)
+    }, wait)
+  }
+
+  debounced.cancel = () => {
+    if (timeout) {
+      clearTimeout(timeout)
+    }
+  }
+
+  return debounced as T & { cancel: () => void }
+}

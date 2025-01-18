214d7db9f7f617be8e4eabf60865a36351bc11a8
Krish-Das <krishdasatgames@gmail.com>
2025-01-18 12:50:17 +0530

    Add ragchat component

--- app/[...url]/page.tsx
+++ app/[...url]/page.tsx
@@ -1,9 +1,21 @@
+// import { ragChat } from "@/lib/ragchat"
+import { reconstructURL } from "@/lib/utils"
+
 type Pageparams = {
-  url: string | string[] | undefined
+  url: string[]
 }
 
 export default async function Page({ params }: { params: unknown }) {
   const { url } = await (params as Promise<Pageparams>)
 
-  return <div>{url}</div>
+  const reconURL = reconstructURL(url)
+  console.log(reconURL)
+
+  // await ragChat.context.add({
+  //   type: "html",
+  //   source: reconURL,
+  //   config: { chunkOverlap: 50, chunkSize: 200 },
+  // })
+
+  return <div>{reconURL}</div>
 }
--- lib/utils.ts
+++ lib/utils.ts
@@ -4,3 +4,8 @@ import { twMerge } from "tailwind-merge"
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs))
 }
+
+export function reconstructURL(url: string[]) {
+  const reconURL = url.map(url => decodeURIComponent(url))
+  return reconURL.join("/")
+}

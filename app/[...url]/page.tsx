5fabfdb4d89eee7a08424cf655ff03fb743329c8
Krish-Das <krishdasatgames@gmail.com>
2025-01-19 13:09:27 +0530

    Add checks to remove repeat request to the database

--- app/[...url]/page.tsx
+++ app/[...url]/page.tsx
@@ -1,4 +1,5 @@
 import { ragChat } from "@/lib/ragchat"
+import { redis } from "@/lib/reddis"
 import { reconstructURL } from "@/lib/utils"
 
 type Pageparams = {
@@ -9,13 +10,21 @@ export default async function Page({ params }: { params: unknown }) {
   const { url } = await (params as Promise<Pageparams>)
 
   const reconURL = reconstructURL(url)
-  console.log(reconURL)
+  const isAlreadyIndexed = await redis.sismember("indexed-urls", reconURL)
 
-  await ragChat.context.add({
-    type: "html",
-    source: reconURL,
-    config: { chunkOverlap: 50, chunkSize: 200 },
-  })
+  console.log("URL:", reconURL)
+  console.log("Indexed:", isAlreadyIndexed)
+
+  if (!isAlreadyIndexed) {
+    console.log("Adding new entry")
+    await ragChat.context.add({
+      type: "html",
+      source: reconURL,
+      config: { chunkOverlap: 50, chunkSize: 200 },
+    })
+
+    redis.sadd("indexed-urls", reconURL)
+  }
 
   return <div>{reconURL}</div>
 }

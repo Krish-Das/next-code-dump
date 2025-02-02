e1cd6155c0f4189ef6bcf1c494d0c3a137f6ba94
Krish-Das <krishdasatgames@gmail.com>
2025-02-02 22:48:48 +0530

    Add catch-all route, types for auth and middleware
    - Add catch-all route required by better-auth
    - Add types for the auth
    - Add a middleware to manage incoming requests

---
+++ middleware.ts
@@ -0,0 +1,26 @@
+import { NextResponse, type NextRequest } from "next/server"
+import { betterFetch } from "@better-fetch/fetch"
+
+import { Session } from "@/lib/types/auth-types"
+
+export default async function authMiddleware(request: NextRequest) {
+  const { data: session } = await betterFetch<Session>(
+    "/api/auth/get-session",
+    {
+      baseURL: request.nextUrl.origin, // NEXT_PUBLIC_URL
+      headers: {
+        //get the cookie from the request
+        cookie: request.headers.get("cookie") || "",
+      },
+    }
+  )
+
+  if (!session) {
+    return NextResponse.redirect(new URL("/", request.url))
+  }
+  return NextResponse.next()
+}
+
+export const config = {
+  matcher: ["/dashboard"],
+}

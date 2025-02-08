503c937c5714291f3f5a9158893d0b0853d565f8
Krish-Das <krishdasatgames@gmail.com>
2025-02-08 21:04:54 +0530

    Add game page

---
+++ app/game/page.tsx
@@ -0,0 +1,10 @@
+import GameCard from "@/components/game/GameCard"
+import { Main } from "@/components/layout/mainwraper"
+
+export default function Page() {
+  return (
+    <Main>
+      <GameCard />
+    </Main>
+  )
+}

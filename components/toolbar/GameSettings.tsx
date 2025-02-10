72de6fef46d1b541285e783330f03957a167095c
Krish-Das <krishdasatgames@gmail.com>
2025-02-10 22:37:17 +0530

    Add game settings tab toggle in the toolbar

--- components/toolbar/GameSettings.tsx
+++ components/toolbar/GameSettings.tsx
@@ -1,38 +1,63 @@
-"use client"
-
+import { Dispatch, SetStateAction } from "react"
 import Link from "next/link"
-import { Button, ButtonGroup } from "@heroui/react"
+import { Button, ButtonGroup, Divider } from "@heroui/react"
 
 import { difficulties } from "@/lib/constants"
 import { type GameDifficulty } from "@/lib/types"
+import { SystemUiconsCross } from "@/components/icons/system-icons"
 
 export default function GameSettings({
   selectedDifficulty,
+  setOpen,
 }: {
   selectedDifficulty: GameDifficulty
+  setOpen: Dispatch<SetStateAction<boolean>>
 }) {
   return (
-    <div className="bg-black p-3">
-      <ButtonGroup>
-        {difficulties.map(difficulty => (
-          <Button
-            key={difficulty}
-            radius="md"
-            className="capitalize"
-            color={difficulty === selectedDifficulty ? "primary" : "default"}
-            // TODO: which variant?
-            // variant={difficulty === selectedDifficulty ? "flat" : "flat"}
-            variant="flat"
-            as={Link}
-            href={{
-              pathname: "/game",
-              query: { d: difficulty },
-            }}
-          >
-            {difficulty}
-          </Button>
-        ))}
-      </ButtonGroup>
+    <div className="flex flex-col gap-6 px-6 py-8">
+      <div className="flex items-center justify-between">
+        <h5 className="touch-none select-none text-base font-bold">Settings</h5>
+
+        <Button
+          radius="full"
+          variant="light"
+          size="sm"
+          isIconOnly
+          onPress={() => setOpen(false)}
+        >
+          <SystemUiconsCross
+            className="size-3.5 text-content4-foreground"
+            strokeWidth={1.75}
+          />
+        </Button>
+      </div>
+
+      <Divider />
+
+      <div className="flex flex-col gap-1.5">
+        <h6 className="touch-none select-none text-sm font-semibold">
+          Difficulty
+        </h6>
+        {/* TODO: Use tabs instead */}
+        <ButtonGroup>
+          {difficulties.map(difficulty => (
+            <Button
+              key={difficulty}
+              radius="md"
+              className="capitalize"
+              color={difficulty === selectedDifficulty ? "primary" : "default"}
+              variant={difficulty === selectedDifficulty ? "solid" : "flat"}
+              as={Link}
+              href={{
+                pathname: "/game",
+                query: { d: difficulty },
+              }}
+            >
+              {difficulty}
+            </Button>
+          ))}
+        </ButtonGroup>
+      </div>
     </div>
   )
 }
--- components/toolbar/Toolbar.tsx
+++ components/toolbar/Toolbar.tsx
@@ -17,77 +17,56 @@ export default function Toolbar({
 }: {
   difficulty: GameDifficulty
 }) {
-  const [isOpen, setIsOpen] = useState(false)
+  const [isOpen, setOpen] = useState(false)
 
   return (
     <nav
-      className="fixed bottom-7 left-1/2 -translate-x-1/2 overflow-hidden border border-content1/80 bg-content2/30 backdrop-blur-md"
+      className="fixed bottom-7 left-1/2 -translate-x-1/2 overflow-hidden border border-content2/40 bg-content2/30 backdrop-blur-md"
       style={{
         borderRadius: isOpen ? "1rem" : "9999px",
       }}
     >
-      {/* TODO: Remove the extra "hidden" when done. */}
-      <div className={cn("h-32 w-full p-1", !isOpen && "hidden", "hidden")}>
-        <div className="flex h-full w-full flex-col rounded-xl bg-content3/40 p-2">
-          <h6 className="text-sm font-semibold leading-loose">Difficulty</h6>
-          <div className="flex gap-2">
+      {!isOpen ? (
+        <div className={cn("flex items-center justify-center gap-4 p-2")}>
+          <div className="flex items-center gap-2">
             <Button color="primary" variant="solid" radius="full" isIconOnly>
               <SolarPauseBold />
             </Button>
+            <p className="mt-[0.3ch] text-lg font-semibold leading-none">
+              02:35
+              <span className="text-bases ml-[0.2ch] font-normal text-content4">
+                s
+              </span>
+            </p>
+          </div>
 
-            <Button variant="flat" radius="full" isIconOnly>
-              <SolarPauseBold />
+          <Divider orientation="vertical" className="h-4 w-0.5 rounded-full" />
+
+          <div className="flex items-center gap-2">
+            <Button
+              color="default"
+              variant="flat"
+              radius="full"
+              isIconOnly
+              onPress={() => setOpen(!isOpen)}
+            >
+              <SolarSettingsBold className="size-4" />
             </Button>
 
-            <Button variant="flat" radius="full" isIconOnly>
-              <SolarPauseBold />
+            <Button
+              color="default"
+              variant="flat"
+              radius="full"
+              isIconOnly
+              onPress={() => setOpen(!isOpen)}
+            >
+              <SolarMenuDotsBold className="size-5" />
             </Button>
           </div>
         </div>
-      </div>
-
-      {/* TODO: Remove the extra "hidden" when done. */}
-      <div
-        className={cn("flex items-center justify-center gap-4 p-2", "hidden")}
-      >
-        <div className="flex items-center gap-2">
-          <Button color="primary" variant="solid" radius="full" isIconOnly>
-            <SolarPauseBold />
-          </Button>
-          <p className="mt-[0.3ch] text-lg font-semibold leading-none">
-            02:35
-            <span className="text-bases ml-[0.2ch] font-normal text-content4">
-              s
-            </span>
-          </p>
-        </div>
-
-        <Divider orientation="vertical" className="h-4 w-0.5 rounded-full" />
-
-        <div className="flex items-center gap-2">
-          <Button
-            color="default"
-            variant="flat"
-            radius="full"
-            isIconOnly
-            onPress={() => setIsOpen(!isOpen)}
-          >
-            <SolarSettingsBold className="size-4" />
-          </Button>
-
-          <Button
-            color="default"
-            variant="flat"
-            radius="full"
-            isIconOnly
-            onPress={() => setIsOpen(!isOpen)}
-          >
-            <SolarMenuDotsBold className="size-5" />
-          </Button>
-        </div>
-      </div>
-
-      <GameSettings selectedDifficulty={difficulty} />
+      ) : (
+        <GameSettings selectedDifficulty={difficulty} setOpen={setOpen} />
+      )}
     </nav>
   )
 }

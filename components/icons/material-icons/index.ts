cea154c65303e0bfbc640119a489f2952d0954b7
Krish-Das <krishdasatgames@gmail.com>
2025-02-12 13:08:26 +0530

    Add Material Icons

---
+++ components/icons/material-icons/index.ts
@@ -0,0 +1,21 @@
+import { MaterialSymbolsArrowBack } from "./MaterialSymbolsArrowBack"
+import {
+  MaterialSymbolsCognition2,
+  MaterialSymbolsCognition2Outline,
+} from "./MaterialSymbolsCognition"
+import { MaterialSymbolsRefresh } from "./MaterialSymbolsRefresh"
+import {
+  MaterialSymbolsSettings,
+  MaterialSymbolsSettingsOutline,
+  MaterialSymbolsSettingsOutlineRounded,
+} from "./MaterialSymbolsSettings"
+
+export {
+  MaterialSymbolsArrowBack,
+  MaterialSymbolsSettingsOutline,
+  MaterialSymbolsSettings,
+  MaterialSymbolsSettingsOutlineRounded,
+  MaterialSymbolsCognition2,
+  MaterialSymbolsCognition2Outline,
+  MaterialSymbolsRefresh,
+}

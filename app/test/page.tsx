5b32c30306ef01fbdb9bfe0e214e31ddd57ab0ba
Krish-Das <krishdasatgames@gmail.com>
2025-02-13 12:31:58 +0530

    Install react-timer-hook

--- app/test/page.tsx
+++ app/test/page.tsx
@@ -3,6 +3,7 @@
 import { useState } from "react"
 import { Divider, Button as NextButton } from "@heroui/react"
 import { Button } from "react-aria-components"
+import { useStopwatch } from "react-timer-hook"
 
 import {
   MaterialSymbolsCognition2Outline,
@@ -12,22 +13,43 @@ import {
 
 export default function Page() {
   const [clickCount, setClickCount] = useState(0)
+  const { minutes, seconds, isRunning, start, pause, reset } = useStopwatch({
+    autoStart: false,
+  })
+
+  const handleCardClick = () => {
+    if (clickCount <= 0 && !isRunning) start()
+
+    setClickCount(v => v + 1)
+    return null
+  }
 
   const Content = () => {
     return (
-      <section className="h-fit w-fit rounded-xl border-2 border-dashed border-red-500 p-3">
-        <Button
-          className="size-96 cursor-default rounded-lg border-none bg-red-500 outline-none rac-focus-visible:ring-4 rac-focus-visible:ring-red-400"
-          onPressEnd={() => setClickCount(v => v + 1)}
-        />
+      <section className="grid h-fit w-fit grid-cols-4 grid-rows-3 gap-3 rounded-xl border-2 border-dashed border-red-500 p-3">
+        {Array.from({ length: 12 }, (_, idx) => (
+          <Button
+            key={idx}
+            className="size-16 cursor-default rounded-lg border-none bg-red-500 outline-none rac-focus-visible:ring-4 rac-focus-visible:ring-red-400"
+            onPressEnd={handleCardClick}
+          />
+        ))}
       </section>
+    )
+  }
+  const _Clicks = () => {
+    return (
+      <div className="fixed left-1/2 top-3 inline-grid -translate-x-1/2 place-content-center rounded-full bg-content4 px-3 py-2 text-medium">
+        <span>{clickCount}</span>
+      </div>
     )
   }
 
   return (
     <div className="grid h-dvh place-content-center">
       <Content />
-      <_Display label={clickCount} />
+      <_Display label={`${minutes}:${seconds}`} />
+      <_Clicks />
     </div>
   )
 }

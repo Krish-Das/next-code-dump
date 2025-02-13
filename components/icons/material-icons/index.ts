1e770ec7cde7025ca2bf3302af040e32b646f6fc
Krish-Das <krishdasatgames@gmail.com>
2025-02-13 11:40:02 +0530

    Add new icons

--- components/icons/material-icons/index.ts
+++ components/icons/material-icons/index.ts
@@ -3,12 +3,21 @@ import {
   MaterialSymbolsCognition2,
   MaterialSymbolsCognition2Outline,
 } from "./MaterialSymbolsCognition"
+import { MaterialSymbolsPause } from "./MaterialSymbolsPause"
+import {
+  MaterialSymbolsPlayArrow,
+  MaterialSymbolsPlayArrowRounded,
+} from "./MaterialSymbolsPlayArrow"
 import { MaterialSymbolsRefresh } from "./MaterialSymbolsRefresh"
 import {
   MaterialSymbolsSettings,
   MaterialSymbolsSettingsOutline,
   MaterialSymbolsSettingsOutlineRounded,
 } from "./MaterialSymbolsSettings"
+import {
+  MaterialSymbolsStop,
+  MaterialSymbolsStopRounded,
+} from "./MaterialSymbolsStop"
 
 export {
   MaterialSymbolsArrowBack,
@@ -18,4 +27,9 @@ export {
   MaterialSymbolsCognition2,
   MaterialSymbolsCognition2Outline,
   MaterialSymbolsRefresh,
+  MaterialSymbolsPause,
+  MaterialSymbolsPlayArrow,
+  MaterialSymbolsPlayArrowRounded,
+  MaterialSymbolsStop,
+  MaterialSymbolsStopRounded,
 }

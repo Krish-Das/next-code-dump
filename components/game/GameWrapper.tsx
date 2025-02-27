a770847efb5792c7f90feaf5c81a38e476bd2d45
Krish-Das <krishdasatgames@gmail.com>
2025-02-27 22:12:02 +0530

    Refactor: to use the new toolbar syntax

--- components/game/GameWrapper.tsx
+++ components/game/GameWrapper.tsx
@@ -2,13 +2,13 @@
 
 // TODO: Remove this client boundary if possible!
 import Game from "@/components/game/Game"
-import Toolbar from "@/components/toolbar/ToolbarV2"
+
+import { Toolbar } from "./Toolbar"
 
 export default function GameWrapper() {
   return (
     <main className="grid h-dvh place-content-center">
       <Game />
-
       <Toolbar />
     </main>
   )
---
+++ components/game/Toolbar.tsx
@@ -0,0 +1,42 @@
+import { Divider } from "@heroui/react"
+
+import { Toolbar } from "@/components/toolbar/ToolbarV3"
+
+import { ColorSchemeSettings, DifficultySettings } from "./GameSettings"
+import GameStatus from "./GameStatus"
+import ImageSources from "./ImageSources"
+import Score from "./Score"
+import ShareGameScore from "./ShareGameScore"
+
+const ToolbarWrapper = () => {
+  return (
+    <Toolbar.Root>
+      <Toolbar.Idle>
+        <GameStatus />
+        <Divider />
+        <div className="flex flex-col gap-1">
+          <Toolbar.Trigger tab="game" />
+          <Toolbar.Trigger tab="settings" />
+        </div>
+      </Toolbar.Idle>
+
+      <Toolbar.Tab tab="game">
+        <Toolbar.Title title="Score">
+          <ShareGameScore />
+        </Toolbar.Title>
+        <Score />
+        <ImageSources />
+      </Toolbar.Tab>
+
+      <Toolbar.Tab tab="settings">
+        <Toolbar.Title title="Settings" />
+        <Toolbar.Separator />
+        <DifficultySettings />
+        <Toolbar.Separator />
+        <ColorSchemeSettings />
+      </Toolbar.Tab>
+    </Toolbar.Root>
+  )
+}
+
+export { ToolbarWrapper as Toolbar }

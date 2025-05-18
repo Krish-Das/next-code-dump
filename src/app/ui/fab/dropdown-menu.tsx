d4059efb86de8430bdb8fb7b1e232e94c3e0947e
Krish-Das <krishdasatgames@gmail.com>
2025-05-18 22:25:30 +0530

    feat: complete the animation

--- src/app/ui/fab/dropdown-menu.tsx
+++ src/app/ui/fab/dropdown-menu.tsx
@@ -1,28 +1,34 @@
 "use client"
 
 import { ReactNode, useState } from "react"
-import { AnimatePresence, motion, Variants } from "motion/react"
+import { AnimatePresence, motion } from "motion/react"
 import { Popover } from "radix-ui"
 
 import { cn } from "@/lib/utils"
 import { IonAddSharp, IonRemoveSharp } from "@/components/icons/ion"
 
+const exitDuration = undefined // as const
+
 export default function Menu() {
   const [open, setOpen] = useState(false)
-  const buttonAnimationVariants: Variants = {
-    open: { opacity: 1, filter: "blur(0px)", rotate: 0 },
-    close: { opacity: 0, filter: "blur(8px)", rotate: 45 },
-  }
-  const newAnimation = {
-    open: { ...buttonAnimationVariants.open, y: "0%" },
-    close: { ...buttonAnimationVariants.close, y: "50%" },
-  }
 
   return (
     <Popover.Root open={open} onOpenChange={setOpen} modal>
       <Popover.Trigger asChild>
         <motion.button
-          variants={buttonAnimationVariants}
+          variants={{
+            open: {
+              opacity: 1,
+              filter: "blur(0px)",
+              rotate: 0,
+              transition: { duration: exitDuration },
+            },
+            close: {
+              opacity: 0,
+              filter: "blur(8px)",
+              rotate: 45,
+            },
+          }}
           animate={open ? "close" : "open"}
           className="inline-flex h-12 w-12 items-center justify-center rounded-full bg-neutral-800 text-xl leading-0 font-medium backdrop-blur-sm"
         >
@@ -34,31 +40,89 @@ export default function Menu() {
       <AnimatePresence>
         {open && (
           <Popover.Portal forceMount>
-            <div>
+            <div className="">
               <motion.div
                 initial={{ opacity: 0 }}
-                exit={{ opacity: 0 }}
+                exit={{ opacity: 0, transition: { duration: exitDuration } }}
                 animate={{ opacity: 1 }}
                 className="absolute inset-0 bg-black/80 backdrop-blur-sm"
               />
 
-              <Popover.Content side="top">
-                <Wrapper>
-                  <Label>Income</Label>
-                  <TransactionAddButton
-                    variants={newAnimation}
-                    type="income"
-                    action={() => alert("Action: income")}
-                  />
+              <Popover.Content side="top" className="space-y-2">
+                {/* Income button */}
+                <Wrapper animate>
+                  <motion.p
+                    variants={{
+                      open: {
+                        opacity: 1,
+                        filter: "blur(0px)",
+                        transition: { delay: 0.2 },
+                        scale: 1,
+                      },
+                      close: {
+                        opacity: 0,
+                        filter: "blur(5px)",
+                        scale: 0.5,
+                        transition: { duration: 0.1 },
+                      },
+                    }}
+                    className="absolute top-1/2 right-16 origin-right -translate-y-1/2 text-right"
+                  >
+                    Income
+                  </motion.p>
+                  <motion.button
+                    variants={{
+                      open: { opacity: 1, filter: "blur(0px)", rotate: 0 },
+                      close: {
+                        opacity: 0,
+                        filter: "blur(8px)",
+                        rotate: 45,
+                        transition: { duration: exitDuration },
+                      },
+                    }}
+                    id="transaction__add-income"
+                    className="relative inline-flex h-12 w-12 items-center justify-center rounded-full bg-neutral-800 text-xl leading-0 font-medium backdrop-blur-sm"
+                  >
+                    <IonAddSharp />
+                  </motion.button>
                 </Wrapper>
 
+                {/* Expense button */}
                 <Wrapper>
-                  <Label>Expense</Label>
-                  <TransactionAddButton
-                    variants={buttonAnimationVariants}
-                    type="expense"
-                    action={() => alert("Action: expense")}
-                  />
+                  <motion.p
+                    variants={{
+                      open: {
+                        opacity: 1,
+                        filter: "blur(0px)",
+                        transition: { delay: 0.18 },
+                        scale: 1,
+                      },
+                      close: {
+                        opacity: 0,
+                        filter: "blur(5px)",
+                        scale: 0.5,
+                        transition: { duration: 0.1 },
+                      },
+                    }}
+                    className="absolute top-1/2 right-16 origin-right -translate-y-1/2 text-right"
+                  >
+                    Expense
+                  </motion.p>
+                  <motion.button
+                    variants={{
+                      open: { opacity: 1, filter: "blur(0px)", rotate: 0 },
+                      close: {
+                        opacity: 0,
+                        filter: "blur(8px)",
+                        rotate: 45,
+                        transition: { duration: exitDuration },
+                      },
+                    }}
+                    id="transaction__add-expense"
+                    className="relative inline-flex h-12 w-12 items-center justify-center rounded-full bg-neutral-800 text-xl leading-0 font-medium backdrop-blur-sm"
+                  >
+                    <IonRemoveSharp />
+                  </motion.button>
                 </Wrapper>
               </Popover.Content>
             </div>
@@ -72,64 +136,31 @@ export default function Menu() {
 const Wrapper = ({
   children,
   className,
+  animate,
 }: {
   children: ReactNode
   className?: string
-}) => {
-  return <div className={cn("relative flex gap-2", className)}>{children}</div>
-}
-
-const Label = ({
-  children,
-  className,
-}: {
-  children: ReactNode
-  className?: string
+  animate?: boolean
 }) => {
   return (
-    <p
-      className={cn(
-        "absolute top-1/2 right-16 -translate-y-1/2 text-right",
-        className
-      )}
-    >
-      {children}
-    </p>
-  )
-}
-
-const TransactionAddButton = ({
-  type,
-  action,
-  className,
-  variants,
-}: {
-  type: "expense" | "income"
-  action: () => void
-  className?: string
-  variants: Variants
-}) => {
-  const isExpense = type === "expense"
-
-  const icon = isExpense ? <IonRemoveSharp /> : <IonAddSharp />
-  const buttonId = isExpense
-    ? "transaction__add-expense"
-    : "transaction__add-income"
-
-  return (
-    <motion.button
-      variants={variants}
+    <motion.div
+      variants={
+        animate
+          ? {
+              open: { y: "0%" },
+              close: {
+                y: "50%",
+                transition: { duration: exitDuration },
+              },
+            }
+          : undefined
+      }
       initial="close"
       exit="close"
       animate="open"
-      id={buttonId}
-      className={cn(
-        "relative inline-flex h-12 w-12 items-center justify-center rounded-full bg-neutral-800 text-xl leading-0 font-medium backdrop-blur-sm",
-        className
-      )}
-      onClick={action}
+      className={cn("relative flex gap-2", className)}
     >
-      {icon}
-    </motion.button>
+      {children}
+    </motion.div>
   )
 }

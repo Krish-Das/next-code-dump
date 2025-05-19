33cedae9a20e98e387db3852caf54e0805e2b08f
Krish-Das <krishdasatgames@gmail.com>
2025-05-19 13:07:37 +0530

    chore: improve the animation

--- src/app/ui/fab/dropdown-menu.tsx
+++ src/app/ui/fab/dropdown-menu.tsx
@@ -1,7 +1,7 @@
 "use client"
 
 import {ReactNode, useState} from "react"
-import {AnimatePresence, motion} from "motion/react"
+import {AnimatePresence, motion, Variants} from "motion/react"
 import {Popover} from "radix-ui"
 
 import {cn} from "@/lib/utils"
@@ -9,33 +9,39 @@ import {IonAddSharp, IonRemoveSharp} from "@/components/icons/ion"
 
 export default function Menu() {
   const [open, setOpen] = useState(false)
+  const iconAnimationVariants = {
+    open: {
+      rotate: 0,
+      opacity: 1,
+      scale: 1,
+      display: "inline",
+      filter: "blur(0px)",
+    },
+    close: {
+      rotate: 45,
+      opacity: 0,
+      scale: 0.5,
+      display: "hidden",
+      filter: "blur(3px)",
+    },
+  } as Variants
 
   return (
     <Popover.Root open={open} onOpenChange={setOpen} modal>
       <Popover.Trigger asChild>
-        <motion.button
-          animate={open ? "close" : "open"}
+        {/* NOTE: */}
+        {/* We don't animate the trigger at all... We just hide it when the
+        popover is open. Instead we animate the button that will be placed on
+        top of the trigger */}
+        <button
           className={cn(
             "inline-flex h-12 w-12 items-center justify-center rounded-full bg-neutral-800 text-xl leading-0 font-medium backdrop-blur-sm",
             open && "hidden"
           )}>
-          <motion.span
-            variants={{
-              open: {
-                rotate: 0,
-                filter: "blur(0px)",
-              },
-              close: {
-                rotate: 45,
-                filter: "blur(5px)",
-                transition: {duration: 0.2},
-              },
-            }}
-            className="svg_container">
-            <IonAddSharp />
-          </motion.span>
-        </motion.button>
+          <IonAddSharp />
+        </button>
       </Popover.Trigger>
+
       <Popover.Anchor />
 
       <AnimatePresence>
@@ -61,7 +67,7 @@ export default function Menu() {
                       open: {
                         opacity: 1,
                         filter: "blur(0px)",
-                        transition: {delay: 0.15},
+                        transition: {delay: 0.135},
                         scale: 1,
                       },
                       close: {
@@ -93,6 +99,7 @@ export default function Menu() {
                         open: {
                           filter: "blur(0px)",
                           scale: 1,
+                          transition: {delay: 0.1},
                         },
                         close: {
                           scale: 0,
@@ -126,34 +133,31 @@ export default function Menu() {
                     className="absolute top-1/2 right-16 origin-right -translate-y-1/2 text-right">
                     Expense
                   </motion.p>
-                  <motion.button
-                    variants={{
-                      open: {
-                        opacity: 1,
-                        transition: {duration: 0.2},
-                      },
-                      close: {
-                        opacity: 0,
-                      },
-                    }}
+
+                  {/* NOTE: */}
+                  {/* Instead of animating the whole button, we'll place the
+                  button on top of the trigger as soon as the popover opens.
+                  And then we'll animate the icons instead. This way it'll
+                  give the illusion that the trigger itself morphed into this
+                  button */}
+                  <button
                     id="transaction__add-expense"
                     className="relative inline-flex h-12 w-12 items-center justify-center rounded-full bg-neutral-800 text-xl leading-0 font-medium backdrop-blur-sm">
                     <motion.span
-                      variants={{
-                        open: {
-                          filter: "blur(0px)",
-                          rotate: 0,
-                          transition: {duration: 0.2},
-                        },
-                        close: {
-                          filter: "blur(5px)",
-                          rotate: 45,
-                        },
-                      }}
-                      className="svg_container">
+                      className="absolute top-1/2 left-1/2 -translate-1/2"
+                      variants={iconAnimationVariants}>
                       <IonRemoveSharp />
                     </motion.span>
-                  </motion.button>
+
+                    <motion.span
+                      className="absolute top-1/2 left-1/2 -translate-1/2"
+                      variants={{
+                        open: iconAnimationVariants.close,
+                        close: iconAnimationVariants.open,
+                      }}>
+                      <IonAddSharp />
+                    </motion.span>
+                  </button>
                 </Wrapper>
               </Popover.Content>
             </div>

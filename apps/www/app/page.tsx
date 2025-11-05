954449e51cc50767d4d1d96514e5060240a5a086
Krish-Das <krishdasatgames@gmail.com>
2025-11-05 23:52:35 +0530

    feat(www): integrate Convex for task management
    Added ConvexProvider to manage tasks in the application and updated the main page to display tasks in JSON format.

--- apps/www/app/page.tsx
+++ apps/www/app/page.tsx
@@ -1,3 +1,16 @@
+"use client"
+
+import { api } from "@repo/backend/convex/_generated/api"
+import { useQuery } from "convex/react"
+
 export default function Page() {
-  return <main>Sup?</main>
+  const tasks = useQuery(api.tasks.get)
+
+  return (
+    <main>
+      <pre>
+        <code lang="json">{JSON.stringify(tasks, null, 2)}</code>
+      </pre>
+    </main>
+  )
 }

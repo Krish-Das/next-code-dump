edddc1365fbeed7d7b23f796e4e84e4de3c2f6b0
Krish-Das <krishdasatgames@gmail.com>
2025-11-08 13:43:12 +0530

    feat(www): integrate Convex with Clerk for authentication
    Updated the Provider component to use ConvexProviderWithClerk for enhanced authentication handling.

--- apps/www/app/provider/index.tsx
+++ apps/www/app/provider/index.tsx
@@ -1,15 +1,20 @@
 "use client"
 
-import { ClerkProvider } from "@clerk/nextjs"
-import { ConvexProvider, ConvexReactClient } from "convex/react"
+import { ClerkProvider, useAuth } from "@clerk/nextjs"
+import { ConvexReactClient } from "convex/react"
+import { ConvexProviderWithClerk } from "convex/react-clerk"
 import type { ReactNode } from "react"
 
 const convex = new ConvexReactClient(process.env.NEXT_PUBLIC_CONVEX_URL || "")
 
 export default function Provider({ children }: { children: ReactNode }) {
   return (
-    <ClerkProvider>
-      <ConvexProvider client={convex}>{children}</ConvexProvider>
+    <ClerkProvider
+      publishableKey={process.env.NEXT_PUBLIC_CLERK_PUBLISHABLE_KEY}
+    >
+      <ConvexProviderWithClerk client={convex} useAuth={useAuth}>
+        {children}
+      </ConvexProviderWithClerk>
     </ClerkProvider>
   )
 }

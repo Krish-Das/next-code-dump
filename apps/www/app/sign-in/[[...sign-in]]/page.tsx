60049d793a376ac151032eb03b79026300113296
Krish-Das <krishdasatgames@gmail.com>
2025-11-09 15:16:41 +0530

    feat(sign-in): customize SignIn component appearance

--- apps/www/app/sign-in/[[...sign-in]]/page.tsx
+++ apps/www/app/sign-in/[[...sign-in]]/page.tsx
@@ -3,7 +3,16 @@ import { SignIn } from "@clerk/nextjs"
 export default function SignInPage() {
   return (
     <div className="flex min-h-screen items-center justify-center">
-      <SignIn />
+      <SignIn
+        appearance={{
+          layout: {
+            socialButtonsPlacement: "bottom",
+            socialButtonsVariant: "blockButton",
+            logoImageUrl:
+              "https://api.iconify.design/icon-park:bytedance-applets.svg",
+          },
+        }}
+      />
     </div>
   )
 }

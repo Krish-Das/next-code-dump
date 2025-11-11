7119602cad5cd301a60efa5279bd6cfbd5394674
Krish-Das <krishdasatgames@gmail.com>
2025-11-11 21:28:34 +0530

    feat(packages/utils): add wait function and Prettify type

--- packages/utils/src/index.ts
+++ packages/utils/src/index.ts
@@ -9,3 +9,22 @@ import { twMerge } from "tailwind-merge"
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs))
 }
+
+/**
+ * Creates a promise that resolves after a specified number of milliseconds
+ * @param milliseconds - The number of milliseconds to wait
+ * @returns A promise that resolves after the specified time
+ */
+export function wait(milliseconds: number) {
+  return new Promise((resolve) => {
+    setTimeout(resolve, milliseconds)
+  })
+}
+
+/**
+ * Forces TypeScript to resolve types to a flat object structure
+ * @template T - The type to prettify
+ */
+export type Prettify<T> = {
+  [K in keyof T]: T[K]
+} & {}
